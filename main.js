ad9b51a668dd506183f843485adacf366f17e609
Valeriy Trusevich <truetnoth@gmail.com>
2018-06-28 10:25:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.onload = function() {
 let augend = document.getElementById('augend').innerHTML; // сбор значений из исходного задания для 1 слагаемого
 let addend = document.getElementById('addend').innerHTML; // сбор значений из исходного задания для 2 слагаемого
 let summary = document.getElementById('summary');
-let summaryNum = String(Number(augend) + Number(addend));
+let summaryNum = String(Number(augend) + Number(addend)); // получаем ответ задания из входных данных
 
 let userInput = document.querySelector('input');
 let correctAugend = document.createElement('span');
@@ -20,18 +20,18 @@ let solvingInput = document.createElement('input');
 let solving = document.querySelector('.solving');
 
 document.querySelector('input').onkeypress = function(e) {
-    if (e.charCode === 0 || e.charCode === 13) {
-        let firstNum = this.value;
-        if (firstNum !== augend) {
-            this.style.color = 'red';
-            document.getElementById('augend').style.backgroundColor = 'orange';
+    if (e.charCode === 0 || e.charCode === 13) { // проверяем клавишу Enter в поле на нажатие
+        let firstNum = this.value; // берем введенное значение
+        if (firstNum !== augend) { // сравниваем его с 1 слагаемым
+            this.style.color = 'red'; // ошибка 
+            document.getElementById('augend').style.backgroundColor = 'orange'; // подсветка ошибки
         } else {
-            this.style.color = '';
-            userInput.remove();
+            this.style.color = ''; 
+            userInput.remove(); // убираем форму для замены на правильный ответ
             document.querySelector('.arrow_augend').appendChild(correctAugend).innerHTML = firstNum;
-            document.querySelector('.number_line').appendChild(addendInput).classList.add('arrow_addend');
-            document.querySelector('.arrow_addend').appendChild(augendInput).classList.add('augendInput');
-            document.querySelector('.augendInput').setAttribute('maxlength', '1');
+            document.querySelector('.number_line').appendChild(addendInput).classList.add('arrow_addend'); // добавляем стрелку
+            document.querySelector('.arrow_addend').appendChild(augendInput).classList.add('augendInput'); // добавляем поле ввода для стрелки 
+            document.querySelector('.augendInput').setAttribute('maxlength', '1'); // выставляем ограничение на количество символов в поле ввода
             document.getElementById('augend').style.backgroundColor = '';
 
             document.querySelector('.augendInput').onkeypress = function(e) {
